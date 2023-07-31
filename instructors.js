ec87124e2148e779de5b40393691a4f9531c4541
Elis Memic <elismemic5@gmail.com>
2023-07-31 11:58:22 +0200

    added data for instructors

---
+++ instructors.js
@@ -0,0 +1,44 @@
+const youngerInstructors = [
+  {
+    name: 'Sara Garcia',
+    title: 'Web Development Instructor',
+    description:
+      'Sara is a talented web developer with a passion for teaching. She enjoys sharing her knowledge of frontend development with aspiring web developers.',
+    img: './images/sara.jpg',
+  },
+  {
+    name: 'Alex Chen',
+    title: 'Data Science Instructor',
+    description:
+      'Alex is a data science enthusiast and loves teaching others how to extract valuable insights from data. His expertise lies in machine learning and data visualization.',
+    img: './images/alex.jpg',
+  },
+  {
+    name: 'Lily Johnson',
+    title: 'UI/UX Design Instructor',
+    description:
+      'Lily is a creative UI/UX designer with a strong background in user-centered design. She enjoys mentoring young designers and helping them develop their skills.',
+    img: './images/lily.jpg',
+  },
+  {
+    name: 'Jake Thompson',
+    title: 'Mobile App Development Instructor',
+    description:
+      'Jake is an experienced mobile app developer who is passionate about building innovative and user-friendly applications. He loves teaching others how to create their own apps.',
+    img: './images/jake.jpg',
+  },
+  {
+    name: 'Ella Martinez',
+    title: 'Game Development Instructor',
+    description:
+      'Ella is a game development expert with a specialization in Unity and Unreal Engine. She enjoys guiding young game developers on their journey to create exciting games.',
+    img: './images/ella.jpg',
+  },
+  {
+    name: 'Max Wilson',
+    title: 'Cybersecurity Instructor',
+    description:
+      'Max is a cybersecurity specialist with extensive knowledge of ethical hacking and network security. He loves to teach students about securing digital assets.',
+    img: './images/max.jpg',
+  },
+];
